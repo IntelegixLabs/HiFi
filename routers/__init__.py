d3fab672dce406c16923de166f6b84ead61de26b
ARNAB DAS <raj713335@gmail.com>
2024-02-13 13:49:43 +0530

    :wrench: Added commodities Exchange API

--- routers/__init__.py
+++ routers/__init__.py
@@ -2,3 +2,4 @@ from routers import auth
 from routers.stocks import core_stocks
 from routers.forex import forex
 from routers.cryptocurrencies import crypto
+from routers.commodities import commodities
---
+++ routers/commodities/commodities.py
@@ -0,0 +1,185 @@
+import os
+import requests
+from fastapi import APIRouter, Depends, HTTPException, Request
+from starlette import status
+from ..auth import get_user_info
+from schemas.userPayload import userPayload
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ALPHA_VANTAGE_URL = os.getenv("ALPHA_VANTAGE_URL")
+ALPHA_VANTAGE_KEY = os.getenv("ALPHA_VANTAGE_KEY")
+
+router = APIRouter(
+    prefix='/commodities',
+    tags=['Commodities API']
+)
+
+
+@router.get('/WTI/query', status_code=status.HTTP_200_OK)
+async def WTI(request: Request, user: userPayload = Depends(get_user_info)):
+    params = request.query_params
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    try:
+        url = ALPHA_VANTAGE_URL + "/query?" + str(params) + "&apikey=" + ALPHA_VANTAGE_KEY
+        payload = {}
+        headers = {'User-Agent': 'request'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+    except Exception as err:
+        return {"message": f"Module - Error - {err}"}, status.HTTP_400_BAD_REQUEST
+
+
+@router.get('/BRENT/query', status_code=status.HTTP_200_OK)
+async def BRENT(request: Request, user: userPayload = Depends(get_user_info)):
+    params = request.query_params
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    try:
+        url = ALPHA_VANTAGE_URL + "/query?" + str(params) + "&apikey=" + ALPHA_VANTAGE_KEY
+        payload = {}
+        headers = {'User-Agent': 'request'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+    except Exception as err:
+        return {"message": f"Module - Error - {err}"}, status.HTTP_400_BAD_REQUEST
+
+
+@router.get('/NATURAL_GAS/query', status_code=status.HTTP_200_OK)
+async def NATURAL_GAS(request: Request, user: userPayload = Depends(get_user_info)):
+    params = request.query_params
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    try:
+        url = ALPHA_VANTAGE_URL + "/query?" + str(params) + "&apikey=" + ALPHA_VANTAGE_KEY
+        payload = {}
+        headers = {'User-Agent': 'request'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+    except Exception as err:
+        return {"message": f"Module - Error - {err}"}, status.HTTP_400_BAD_REQUEST
+
+
+@router.get('/COPPER/query', status_code=status.HTTP_200_OK)
+async def COPPER(request: Request, user: userPayload = Depends(get_user_info)):
+    params = request.query_params
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    try:
+        url = ALPHA_VANTAGE_URL + "/query?" + str(params) + "&apikey=" + ALPHA_VANTAGE_KEY
+        payload = {}
+        headers = {'User-Agent': 'request'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+    except Exception as err:
+        return {"message": f"Module - Error - {err}"}, status.HTTP_400_BAD_REQUEST
+
+
+@router.get('/ALUMINUM/query', status_code=status.HTTP_200_OK)
+async def ALUMINUM(request: Request, user: userPayload = Depends(get_user_info)):
+    params = request.query_params
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    try:
+        url = ALPHA_VANTAGE_URL + "/query?" + str(params) + "&apikey=" + ALPHA_VANTAGE_KEY
+        payload = {}
+        headers = {'User-Agent': 'request'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+    except Exception as err:
+        return {"message": f"Module - Error - {err}"}, status.HTTP_400_BAD_REQUEST
+
+
+@router.get('/WHEAT/query', status_code=status.HTTP_200_OK)
+async def WHEAT(request: Request, user: userPayload = Depends(get_user_info)):
+    params = request.query_params
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    try:
+        url = ALPHA_VANTAGE_URL + "/query?" + str(params) + "&apikey=" + ALPHA_VANTAGE_KEY
+        payload = {}
+        headers = {'User-Agent': 'request'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+    except Exception as err:
+        return {"message": f"Module - Error - {err}"}, status.HTTP_400_BAD_REQUEST
+
+
+@router.get('/CORN/query', status_code=status.HTTP_200_OK)
+async def CORN(request: Request, user: userPayload = Depends(get_user_info)):
+    params = request.query_params
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    try:
+        url = ALPHA_VANTAGE_URL + "/query?" + str(params) + "&apikey=" + ALPHA_VANTAGE_KEY
+        payload = {}
+        headers = {'User-Agent': 'request'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+    except Exception as err:
+        return {"message": f"Module - Error - {err}"}, status.HTTP_400_BAD_REQUEST
+
+
+@router.get('/COTTON/query', status_code=status.HTTP_200_OK)
+async def COTTON(request: Request, user: userPayload = Depends(get_user_info)):
+    params = request.query_params
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    try:
+        url = ALPHA_VANTAGE_URL + "/query?" + str(params) + "&apikey=" + ALPHA_VANTAGE_KEY
+        payload = {}
+        headers = {'User-Agent': 'request'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+    except Exception as err:
+        return {"message": f"Module - Error - {err}"}, status.HTTP_400_BAD_REQUEST
+
+
+@router.get('/SUGAR/query', status_code=status.HTTP_200_OK)
+async def SUGAR(request: Request, user: userPayload = Depends(get_user_info)):
+    params = request.query_params
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    try:
+        url = ALPHA_VANTAGE_URL + "/query?" + str(params) + "&apikey=" + ALPHA_VANTAGE_KEY
+        payload = {}
+        headers = {'User-Agent': 'request'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+    except Exception as err:
+        return {"message": f"Module - Error - {err}"}, status.HTTP_400_BAD_REQUEST
+
+
+@router.get('/COFFEE/query', status_code=status.HTTP_200_OK)
+async def COFFEE(request: Request, user: userPayload = Depends(get_user_info)):
+    params = request.query_params
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    try:
+        url = ALPHA_VANTAGE_URL + "/query?" + str(params) + "&apikey=" + ALPHA_VANTAGE_KEY
+        payload = {}
+        headers = {'User-Agent': 'request'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+    except Exception as err:
+        return {"message": f"Module - Error - {err}"}, status.HTTP_400_BAD_REQUEST
+
+
+@router.get('/ALL_COMMODITIES/query', status_code=status.HTTP_200_OK)
+async def ALL_COMMODITIES(request: Request, user: userPayload = Depends(get_user_info)):
+    params = request.query_params
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    try:
+        url = ALPHA_VANTAGE_URL + "/query?" + str(params) + "&apikey=" + ALPHA_VANTAGE_KEY
+        payload = {}
+        headers = {'User-Agent': 'request'}
+        response = requests.request("GET", url, headers=headers, data=payload)
+        return response.json()
+    except Exception as err:
+        return {"message": f"Module - Error - {err}"}, status.HTTP_400_BAD_REQUEST
+
+
