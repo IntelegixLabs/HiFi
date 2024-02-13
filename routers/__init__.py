b4aa054bd6f8d7ef8eec393dfd05ef1844bfa26d
ARNAB DAS <raj713335@gmail.com>
2024-02-13 22:54:46 +0530

    :wrench: Adding News Analytics API

--- routers/__init__.py
+++ routers/__init__.py
@@ -4,3 +4,4 @@ from routers.forex import forex
 from routers.cryptocurrencies import crypto
 from routers.commodities import commodities
 from routers.economic_indicators import economic_indicators
+from routers.news_insights import news_insights
