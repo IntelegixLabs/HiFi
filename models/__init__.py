e281fcae11c47d696116bb7952c0fb396aa92b10
ARNAB DAS <raj713335@gmail.com>
2024-03-04 14:09:17 +0530

    :wrench: Adding profile Image functionality

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from config.database import Base
 from models.userProfileModel import userProfileModel
 from models.eKycModel import eKycModel
 from models.panModel import panModel
+from models.profileImageModel import profileImageModel
--- models/profileImageModel.py
+++ models/profileImageModel.py
@@ -3,8 +3,8 @@ from sqlalchemy import Column, String, ForeignKey
 from datetime import datetime
 
 
-class panModel(Base):
-    __tablename__ = 'panImage'
+class profileImageModel(Base):
+    __tablename__ = 'profileImage'
 
     userId = Column(String, ForeignKey("userProfile.userId"), primary_key=True, unique=True, nullable=False, index=True)
     file_url = Column(String, default=datetime.now)
